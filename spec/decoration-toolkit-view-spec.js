ada5d01a769140fb2dac39145c508eee5a47e617
Ash Wilson <smashwilson@github.com>
2019-01-28 13:05:15 -0500

    apm init

---
+++ spec/decoration-toolkit-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DecorationToolkitView from '../lib/decoration-toolkit-view';
+
+describe('DecorationToolkitView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
